f725c86143672c45ad96001f38745796183cf0cc
Alex Oliveira <alex.borgesoliveira@gmail.com>
2020-07-29 22:55:55 -0300

    Inciando os exemplos dos tipos de dados Numeros
    Close #1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# exercise_and_examples
+# Exercise_and_examples
 Utilizado para fazer exercícios durante o processo de aprendizagem
+
+# Tipos de dados Básicos 
+- Números --> int , float , long , complex
+
+    
+    
+    
+    
+ Dados utilizados do site:
+ https://www.python.org
---
+++ exercicios/numeros.py
@@ -0,0 +1,14 @@
+# Variavel do tipo Inteiro
+Num_int = 1
+
+print(f'Tipo da classe Num_int  {type(Num_int)}')
+print(f'Valor da variavel :  {Num_int}')
+print(f'Somando valor da variavle + o núemro 1 = {Num_int + 1}')
+print()
+
+# Variavel do tipo Flutuantes
+Num_float = 1.23
+
+print(f'Tipo da clase Num_floar {type(Num_float)}')
+print(f'Valor da variavel : {Num_float}')
+print(f'Soando o valor da variavel + o numero 1.23 = {Num_float + 1.23}')
